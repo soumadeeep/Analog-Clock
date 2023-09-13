d1a57c41246529639283c207b34ae47c4e43f903
Soumadeep Barik <144767955+soumadeeep@users.noreply.github.com>
2023-09-13 22:23:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function show_clock(){
+let h=document.getElementsByClassName('hr')[0];
+let m=document.getElementsByClassName('mn')[0];
+let s=document.getElementsByClassName('ss')[0];
+
+let date=new Date();
+let hours=date.getHours();
+let minutes=date.getMinutes();
+let second=date.getSeconds();
+
+h.style.transform = `rotate(${30*hours+minutes/2}deg)`;
+m.style.transform = `rotate(${6*minutes}deg)`;
+s.style.transform = `rotate(${6*second}deg)`;
+
+}
+setInterval(show_clock,1000);
